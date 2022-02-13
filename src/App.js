131ccfa4c5607842efb84cdfe85a99f778a1f891
Harim Tanko <harimtanko@gmail.com>
2022-02-13 12:44:18 +0000

    set up state values for name of input, list isEditing, editId, alert

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  //set state for the form/input and also set state
+  // for the list that we will display
+  // set state for if were editing or not (isEditing)
+
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+
+  //set state for editId to be able to know which
+  //item we are editing
+  const [editId, setEditId] = useState(null);
+
+  //set state for the alert
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  return <section className="section-center">grocery bud setup</section>;
 }
 
-export default App
+export default App;
